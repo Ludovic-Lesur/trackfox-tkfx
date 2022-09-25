5621ce5d02a7380429a473e0211b39fa8656a45c
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2022-09-25 07:58:36 +0200

    Add error stack driver.

---
+++ inc/applicative/error.h
@@ -0,0 +1,96 @@
+/*
+ * error.h
+ *
+ *  Created on: 25 sep. 2022
+ *      Author: Ludo
+ */
+
+#ifndef __ERROR_H__
+#define __ERROR_H__
+
+// Peripherals.
+#include "adc.h"
+#include "aes.h"
+#include "flash.h"
+#include "i2c.h"
+#include "iwdg.h"
+#include "lptim.h"
+#include "lpuart.h"
+#include "nvm.h"
+#include "rcc.h"
+#include "rtc.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+// Utils.
+#include "math.h"
+#include "parser.h"
+#include "string.h"
+// Components.
+#include "mma8653fc.h"
+#include "neom8n.h"
+#include "s2lp.h"
+#include "sht3x.h"
+
+/*** ERROR macros ***/
+
+#define ERROR_STACK_DEPTH	6
+
+/*** ERROR structures ***/
+
+typedef enum {
+	SUCCESS = 0,
+	ERROR_BUSY,
+	ERROR_SIGFOX_RC,
+	// Peripherals.
+	ERROR_BASE_ADC1 = 0x0100,
+	ERROR_BASE_AES = (ERROR_BASE_ADC1 + ADC_ERROR_BASE_LAST),
+	ERROR_BASE_FLASH = (ERROR_BASE_AES + AES_ERROR_BASE_LAST),
+	ERROR_BASE_I2C1 = (ERROR_BASE_FLASH + FLASH_ERROR_BASE_LAST),
+	ERROR_BASE_IWDG = (ERROR_BASE_I2C1 + I2C_ERROR_BASE_LAST),
+	ERROR_BASE_LPTIM1 = (ERROR_BASE_IWDG + IWDG_ERROR_BASE_LAST),
+	ERROR_BASE_LPUART1 = (ERROR_BASE_LPTIM1 + LPTIM_ERROR_BASE_LAST),
+	ERROR_BASE_NVM = (ERROR_BASE_LPUART1 + LPUART_ERROR_BASE_LAST),
+	ERROR_BASE_RCC = (ERROR_BASE_NVM + NVM_ERROR_BASE_LAST),
+	ERROR_BASE_RTC = (ERROR_BASE_RCC + RCC_ERROR_BASE_LAST),
+	ERROR_BASE_SPI1 = (ERROR_BASE_RTC + RTC_ERROR_BASE_LAST),
+	ERROR_BASE_SPI2 = (ERROR_BASE_SPI1 + SPI_ERROR_BASE_LAST),
+	ERROR_BASE_TIM21 = (ERROR_BASE_SPI2 + SPI_ERROR_BASE_LAST),
+	ERROR_BASE_USART = (ERROR_BASE_TIM21 + TIM_ERROR_BASE_LAST),
+	// Utils.
+	ERROR_BASE_MATH = (ERROR_BASE_USART + USART_ERROR_BASE_LAST),
+	ERROR_BASE_PARSER = (ERROR_BASE_MATH + MATH_ERROR_BASE_LAST),
+	ERROR_BASE_STRING = (ERROR_BASE_PARSER + PARSER_ERROR_BASE_LAST),
+	// Components.
+	ERROR_BASE_MMA8653FC = (ERROR_BASE_STRING + STRING_ERROR_BASE_LAST),
+	ERROR_BASE_NEOM8N = (ERROR_BASE_MMA8653FC + MMA8653FC_ERROR_BASE_LAST),
+	ERROR_BASE_S2LP = (ERROR_BASE_NEOM8N + NEOM8N_ERROR_BASE_LAST),
+	ERROR_BASE_SHT3X = (ERROR_BASE_NEOM8N + NEOM8N_ERROR_BASE_LAST),
+	// Libraries.
+	ERROR_BASE_SIGFOX = (ERROR_BASE_SHT3X + SHT3X_ERROR_BASE_LAST),
+	// Last index.
+	ERROR_BASE_LAST
+} ERROR_t;
+
+/*** ERROR functions ***/
+
+void ERROR_stack_init(void);
+void ERROR_stack_add(ERROR_t status);
+void ERROR_stack_read(ERROR_t* error_stack);
+unsigned char ERROR_stack_is_empty(void);
+
+#define ERROR_status_check(status, success, error_base) { \
+	if (status != success) { \
+		ERROR_stack_add(error_base + status); \
+	} \
+}
+
+#define ERROR_status_check_print(status, success, error_base) { \
+	if (status != success) { \
+		ERROR_stack_add(error_base + status); \
+		AT_print_status(error_base + status); \
+		goto errors; \
+	} \
+}
+
+#endif /* __ERROR_H__ */
---
+++ src/applicative/error.c
@@ -0,0 +1,73 @@
+/*
+ * error.c
+ *
+ *  Created on: 25 sep. 2022
+ *      Author: Ludo
+ */
+
+#include "error.h"
+
+/*** ERROR local global variables ***/
+
+static ERROR_t error_stack[ERROR_STACK_DEPTH];
+static unsigned int error_stack_idx = 0;
+
+/*** ERROR functions ***/
+
+/* INIT ERROR STACK.
+ * @param:	None.
+ * @return:	None.
+ */
+void ERROR_stack_init(void) {
+	// Reset stack.
+	for (error_stack_idx=0 ; error_stack_idx<ERROR_STACK_DEPTH ; error_stack_idx++) error_stack[error_stack_idx] = SUCCESS;
+	error_stack_idx = 0;
+}
+
+/* ADD STATUS TO ERROR STACK.
+ * @param status:	Error code to add.
+ * @return:			None.
+ */
+void ERROR_stack_add(ERROR_t status) {
+	// Add error code.
+	error_stack[error_stack_idx] = status;
+	// Increment index.
+	error_stack_idx++;
+	if (error_stack_idx >= ERROR_STACK_DEPTH) {
+		error_stack_idx = 0;
+	}
+}
+
+/* READ ERROR STACK.
+ * @param error_stack_ptr:	Pointer to the error stack.
+ * @return:					None.
+ */
+void ERROR_stack_read(ERROR_t* error_stack_ptr) {
+	// Local variables.
+	unsigned int idx = 0;
+	unsigned int chrono_idx = error_stack_idx;
+	// Fill table in chronological order.
+	for (idx=0 ; idx<ERROR_STACK_DEPTH ; idx++) {
+		// Compute previous index.
+		chrono_idx = (chrono_idx == 0) ? (ERROR_STACK_DEPTH - 1) : (chrono_idx - 1);
+		error_stack_ptr[idx] = error_stack[chrono_idx];
+	}
+}
+
+/* CHECK IF ERROR STACK IS EMPTY..
+ * @param:	None.
+ * @return:	1 if the stack is empty (no error), 0 otherwise.
+ */
+unsigned char ERROR_stack_is_empty(void) {
+	// Local variables.
+	unsigned char is_empty = 1;
+	unsigned char idx = 0;
+	// Loop on stack.
+	for (idx=0 ; idx<ERROR_STACK_DEPTH ; idx++) {
+		if (error_stack[idx] != SUCCESS) {
+			is_empty = 0;
+			break;
+		}
+	}
+	return is_empty;
+}
