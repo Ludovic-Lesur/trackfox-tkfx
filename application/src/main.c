5ede26a7a30f3b08970f1d70974236ca45594940
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-08-05 19:03:08 +0200

    Increase start detection sensitivity in moto mode.

--- application/src/main.c
+++ application/src/main.c
@@ -178,7 +178,7 @@ static const TKFX_configuration_t TKFX_CONFIG = { 10, 150, 300, 86400 };
 static const TKFX_configuration_t TKFX_CONFIG = { 5, 300, 600, 86400 };
 #endif
 #ifdef TKFX_MODE_MOTO
-static const TKFX_configuration_t TKFX_CONFIG = { 5, 600, 300, 86400 };
+static const TKFX_configuration_t TKFX_CONFIG = { 2, 600, 300, 86400 };
 #endif
 #endif
 
